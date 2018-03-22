c10f0c9c22bbe8feb8a1effbde7d9de875d74f2e
James Curran <james.curran@gmail.com>
2018-03-22 16:42:20 -0400

    Added comment to unit test.

--- MicroRuleEngine.Tests/DataRowTest.cs
+++ MicroRuleEngine.Tests/DataRowTest.cs
@@ -11,7 +11,7 @@ namespace MicroRuleEngine.Tests
         public void DataRowTest()
         {
             var dr = GetDataRow();
-
+            // (int) dr["Column2"] == 123 &&  (string) dr["Column1"] == "Test"
             Rule rule = DataRule.Create<int>("Column2", mreOperator.Equal, "123") & DataRule.Create<string>("Column1", mreOperator.Equal, "Test");
 
             MRE engine = new MRE();
